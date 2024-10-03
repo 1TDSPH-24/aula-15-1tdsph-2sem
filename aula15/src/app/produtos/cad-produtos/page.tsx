f883fca43dcd2ee816ce9b6645285b3280db87e8
gustavodscruz <gustavodiasdsc@gmail.com>
2024-10-03 09:33:38 -0300

    Implemtnados rediret com useRouter, tratamento de erro no envio de dados, fravação de um novo produto de fato na base com post através do fetch

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -1,22 +1,46 @@
 "use client";
 
 import { TipoProduto } from "@/types";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function CadProdutos() {
+  const navigate = useRouter();
   const [produto, setProduto] = useState<TipoProduto>({
     id: 0,
     nome: "",
     preco: 0,
     estoque: 0,
   });
+  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault();
+    try{
+      const response = await fetch("https://localhost:3000/api/base-produtos", {
+        method: "POST",
+        headers: {
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(produto),
+      });
+      if (response.ok){
+        setProduto({id:0, nome:"", preco:0, estoque:0})
+        alert("Produto cadastrado com sucesso!");
+        navigate.push("/produtos");
+      }else{
+        alert("Erro no cadastrar produto!");
+      }
+    }
+    catch(error){
+      console.error("Erro ao cadastrar produto: ", error);
+    }
+  }
 
   return (
     <div>
       <h1>Cadastro de Produtos</h1>
 
       <div>
-        <form action="" method="post" onSubmit={}>
+        <form action="" method="post" onSubmit={handleSubmit}>
           <div>
             <label>Nome do produto:</label>
             <input
@@ -54,7 +78,7 @@ export default function CadProdutos() {
               }
             />
           </div>
-          <button onSubmit={}></button>
+          <button className="border border-black rounded bg-slate-500 text-slate-200 font-semibold px-5 py-2 my-3 hover:bg-red-500 transition-all duration-300" type="submit">Cadastrar</button>
         </form>
         <p>Nome do Produto: {produto.nome}</p>
         <p>Preço do produto: {produto.preco}</p>
