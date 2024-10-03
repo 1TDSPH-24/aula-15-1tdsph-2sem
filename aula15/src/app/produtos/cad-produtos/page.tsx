7ee36611e1599444915c7a886253bd62f899701e
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2024-10-03 09:33:25 -0300

    Implemetados Redirect com useRouter, tratamento de erro no envio de dados, gravação de um novo produto de fato na base com POST através do fetch.

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import { TipoProduto } from "@/types"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function CadProdutos() {
+
+  const navigate = useRouter();
 
   //Armazenando o valor de estado dos inputs.
   const [produto, setProduto] = useState<TipoProduto>({
@@ -13,12 +16,37 @@ export default function CadProdutos() {
     estoque: 0,
   })
 
+  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=> {
+    
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/api/base-produtos", {
+        method:"POST",
+        headers:{"Content-type":"application/json",}, 
+        body: JSON.stringify(produto),
+      })
+
+      if(response.ok){
+        setProduto({id: 0, nome:"",preco:0, estoque:0 });
+        alert("Produto cadastrado com sucesso!")
+        // Redirecionando o usuário para a página de produtos.
+        navigate.push("/produtos");
+      }else{
+        alert("Erro ao cadastrar Produto!")
+      }
+    } catch (error) {
+      console.log("Erro ao cadastrar produto: ", error);
+    }
+
+  }
+
   return (
     <div>
         <h1>Cadastro de Produtos</h1>
 
           <div>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div>
                 <label htmlFor="idNome">Nome do produto</label>
                 <input type="text" name="nome"  id="idNome" value={produto.nome} onChange={(e)=> setProduto({...produto,nome:e.target.value})} />
@@ -31,7 +59,9 @@ export default function CadProdutos() {
                 <label htmlFor="idEstoque">Estoque do produto</label>
                 <input type="number" name="estoque"  id="idEstoque" value={produto.estoque} onChange={(e)=> setProduto({...produto,estoque: parseInt(e.target.value)})} />
               </div>
-              
+              <div>
+                <button type="submit">Cadastrar</button>
+              </div>
             </form>
           </div>
     </div>
