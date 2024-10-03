344fdb9c132bb1ec19067cee6d8932fc0437045e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-03 08:44:05 -0300

    Criando a implementação do REQUEST e corrigindo a tipagem do types TipoProduto.

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -1,12 +1,25 @@
+"use client"
+
+import { TipoProduto } from "@/types"
+import { useState } from "react"
 
 export default function CadProdutos() {
+
+  //Armazenando o valor de estado dos inputs.
+  const [produto, setProduto] = useState<TipoProduto>({
+    id:0,
+    nome: "",
+    preco: 0,
+    estoque: 0,
+  })
+
   return (
     <div>
         <h1>Cadastro de Produtos</h1>
 
           <div>
             <form method="GET">
-              
+
             </form>
           </div>
 
