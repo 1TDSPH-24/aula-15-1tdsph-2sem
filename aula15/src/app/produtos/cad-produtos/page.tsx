570d1bbf3d989bac0ebf7a80f35f576e6deda389
cotete <feliperibeirots@gmail.com>
2024-10-03 09:32:42 -0300

    implementação do post do produto

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { TipoProduto } from "@/types"
+import { useRouter } from "next/navigation";
 import { useState } from "react"
 
 export default function CadProdutos() {
@@ -12,13 +13,32 @@ export default function CadProdutos() {
     preco: 0,
     estoque: 0,
   });
+  const navigate = useRouter();
+
+  const handleSubmit= async(e : React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    try{
+      const response = await fetch("http://localhost:3000/api/base-produtos",
+        {method:"POST",
+          headers:{
+            "Content-Type":"application/json",body: JSON.stringify(produto),
+        }})
+        if(response.ok){
+          setProduto({id:0,nome:"",preco:0,estoque:0})
+          alert("Produto cadastrado com sucesso!")
+          navigate.push("/produtos")
+        }
+    }catch(error){
+      console.error("Produto não adicionado")
+    }
+  }
 
   return (
     <div>
         <h1>Cadastro de Produtos</h1>
 
           <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                   <label htmlFor="idNome">Nome do Produto:</label>
                   <input type="text" name="nome" id="idNome" value={produto.nome} onChange={(e)=> setProduto({...produto,nome:e.target.value})}/>
@@ -30,6 +50,9 @@ export default function CadProdutos() {
                 <div>
                   <label htmlFor="idEstoque">Estoque do Produto:</label>
                   <input type="number" name="estoque" id="idEstoque" value={produto.estoque} onChange={(e)=> setProduto({...produto,estoque: parseInt(e.target.value) })}/>
+                </div>
+                <div>
+                  <button type="submit">Cadastrar</button>
                 </div>
             </form>
           </div>
