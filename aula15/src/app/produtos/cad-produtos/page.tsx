c2e51fa6a8612ddd14d26727bc6ebf24349a920a
Carlos-Eduardo-Rabelo <caduvera220@gmail.com>
2024-10-03 08:46:30 -0300

    Requisição POST criada e testada.

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -4,6 +4,14 @@ export default function CadProdutos() {
   return (
     <div>
         <h1>Cadastro de Produtos</h1>
+
+        <div>
+          <form action="" method="post">
+            
+          </form>
+        </div>
+
+
     </div>
   )
 }
