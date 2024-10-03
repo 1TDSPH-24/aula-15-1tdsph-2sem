33b3d4c91bfa91a5f661d8011d8ac17c649f4b1b
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-03 09:47:35 -0300

    Finalizando o request POST utilizando plugin FORM de tailwind estilizando o FORM.

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -51,21 +51,21 @@ export default function CadProdutos() {
         <h1>Cadastro de Produtos</h1>
 
           <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
                 <div>
-                  <label htmlFor="idNome">Nome do Produto:</label>
-                  <input type="text" name="nome" id="idNome" value={produto.nome} onChange={(e)=> setProduto({...produto,nome:e.target.value})}/>
+                  <label htmlFor="idNome" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do Produto:</label>
+                  <input type="text" name="nome" id="idNome" value={produto.nome} onChange={(e)=> setProduto({...produto,nome:e.target.value})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nome do Produto"/>
                 </div>
                 <div>
-                  <label htmlFor="idPreco">Preço do Produto:</label>
-                  <input type="number" name="preco" id="idPreco" value={produto.preco} onChange={(e)=> setProduto({...produto,preco: parseFloat(e.target.value) })}/>
+                  <label htmlFor="idPreco" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preço do Produto:</label>
+                  <input type="number" name="preco" id="idPreco" value={produto.preco} onChange={(e)=> setProduto({...produto,preco: parseFloat(e.target.value) })} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                 </div>
                 <div>
                   <label htmlFor="idEstoque">Estoque do Produto:</label>
-                  <input type="number" name="estoque" id="idEstoque" value={produto.estoque} onChange={(e)=> setProduto({...produto,estoque: parseInt(e.target.value) })}/>
+                  <input type="number" name="estoque" id="idEstoque" value={produto.estoque} onChange={(e)=> setProduto({...produto,estoque: parseInt(e.target.value) })} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                 </div>
                 <div>
-                  <button type="submit">Cadastrar</button>
+                  <button type="submit"  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</button>
                 </div>
             </form>
           </div>
