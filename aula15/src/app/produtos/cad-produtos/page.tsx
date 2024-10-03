a11a894eb89d7c92342f64d740a03eaabd7dc88a
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-03 09:08:43 -0300

    Preenchimento do form cocluído com useState e onChange.

--- aula15/src/app/produtos/cad-produtos/page.tsx
+++ aula15/src/app/produtos/cad-produtos/page.tsx
@@ -11,18 +11,29 @@ export default function CadProdutos() {
     nome: "",
     preco: 0,
     estoque: 0,
-  })
+  });
 
   return (
     <div>
         <h1>Cadastro de Produtos</h1>
 
           <div>
-            <form method="GET">
-
+            <form>
+                <div>
+                  <label htmlFor="idNome">Nome do Produto:</label>
+                  <input type="text" name="nome" id="idNome" value={produto.nome} onChange={(e)=> setProduto({...produto,nome:e.target.value})}/>
+                </div>
+                <div>
+                  <label htmlFor="idPreco">Preço do Produto:</label>
+                  <input type="number" name="preco" id="idPreco" value={produto.preco} onChange={(e)=> setProduto({...produto,preco: parseFloat(e.target.value) })}/>
+                </div>
+                <div>
+                  <label htmlFor="idEstoque">Estoque do Produto:</label>
+                  <input type="number" name="estoque" id="idEstoque" value={produto.estoque} onChange={(e)=> setProduto({...produto,estoque: parseInt(e.target.value) })}/>
+                </div>
             </form>
           </div>
-
+ 
     </div>
   )
 }
